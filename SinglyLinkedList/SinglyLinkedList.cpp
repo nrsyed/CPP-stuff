d05bb070e8fbdec096a82180f26f84582ae06914
najam <nsyed88@gmail.com>
2018-06-09 18:59:42 -0400

    Add SinglyLinkedList header and source

---
+++ SinglyLinkedList/SinglyLinkedList.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+
+template <class T>
+SinglyLinkedList<T>::SinglyLinkedList() {
+	head = nullptr;
+	length = 0;
+}
+
+template <class T>
+SinglyLinkedList<T>::~SinglyLinkedList() {
+	Node* node = head;
+	Node* next;
+	while (node) {
+		next = node->next;
+		delete node;
+		node = next;
+	}
+}
+
+template <class T>
+int SinglyLinkedList<T>::size() {
+	return length;
+}
+
+template <class T>
+void SinglyLinkedList<T>::print() {
+	Node* node = head;
+	while (node) {
+		std::cout << node->data << " ";
+		node = node->next;
+	}
+	std::cout << std::endl;
+}
+
+template <class T>
+void SinglyLinkedList<T>::insert(int index, T item) {
+	index = index < 0 ? length + index : index;
+	if (index < 0 || index > length)
+		throw std::out_of_range("List index out of range");
+	if (!head) {
+		head = new Node(item);
+	} else {
+		Node *node = head;
+		Node **pp = &head;
+		int i = 0;
+		while (i < index) {
+			pp = &(node->next);
+			node = node->next;
+			i++;
+		}
+		*pp = new Node(item);
+		((*pp)->next) = node;
+	}
+	length++;
+}
+
+template <class T>
+void SinglyLinkedList<T>::push(T item) {
+    insert(length, item);
+}
+
+template <class T>
+T SinglyLinkedList<T>::remove(int index) {
+	index = index < 0 ? length + index : index;
+	if (index < 0 || index >= length || !head)
+		throw std::out_of_range("List index out of range");
+
+	Node *node = head;
+	Node **pp = &head;
+	int i = 0;
+	while (i < index) {
+		pp = &(node->next);
+		node = node->next;
+		i++;
+	}
+	*pp = node->next;
+	length--;
+	T removedData = node->data;
+	delete node;
+	return removedData;
+}
+
+template <class T>
+T SinglyLinkedList<T>::pop() {
+	return remove(length - 1);
+}
+
+template <class T>
+void SinglyLinkedList<T>::swap(int index1, int index2) {
+	index1 = index1 < 0 ? length + index1 : index1;
+	index2 = index2 < 0 ? length + index2 : index2;
+	if (index1 < 0 || index1 >= length || index2 < 0 || index2 >= length) {
+		throw std::out_of_range("List index out of range");
+	} else if (index1 == index2) {
+		return;
+	}
+
+	int indexLow = index1 < index2 ? index1 : index2;
+	int indexHigh = indexLow == index1 ? index2 : index1;
+
+	Node* ptrLow = head;
+	Node* ptrHigh = head;
+	Node** ppLow = &head;
+	Node** ppHigh = &head;
+	Node* temp;
+
+	int i;
+	for (i = 0; i < indexLow; i++) {
+		ppLow = &(ptrLow->next);
+		ptrLow = ptrLow->next;
+	}
+
+	ptrHigh = ptrLow;
+	ppHigh = ppLow;
+	for (; i < indexHigh; i++) {
+		ppHigh = &(ptrHigh->next);
+		ptrHigh = ptrHigh->next;
+	}
+
+	temp = ptrLow->next;
+	*ppLow = ptrHigh;
+	ptrLow->next = ptrHigh->next;
+	if (temp == ptrHigh) {
+		ptrHigh->next = ptrLow;
+	} else {
+		ptrHigh->next = temp;
+		*ppHigh = ptrLow;
+	}
+}
+
+template <class T>
+const T& SinglyLinkedList<T>::operator[](int index) const {
+	Node* node = head;
+	for (int i = 0; i < index; i++) {
+		node = node->next;
+	}
+	return node->data;
+}
+
+template <class T>
+T& SinglyLinkedList<T>::operator[](int index) {
+	Node* node = head;
+	for (int i = 0; i < index; i++) {
+		node = node->next;
+	}
+	return node->data;
+}
---
+++ SinglyLinkedList/SinglyLinkedList.hpp
@@ -0,0 +1,112 @@
+#ifndef SINGLY_LINKED_LIST_H
+#define SINGLY_LINKED_LIST_H
+
+/*!
+@brief Simple templatized implementation of a singly linked list.
+*/
+template <class T>
+class SinglyLinkedList {
+    private:
+    struct Node {
+        T data;
+        Node *next;
+        Node(T x) : data(x), next(nullptr) {};
+    };
+    Node *head;
+    int length;
+
+    public:
+
+	/*! @brief Return the item at \p index.
+
+		Example:
+		@code
+		SinglyLinkedList<int> s;
+		s.push(10);
+		s.push(12);
+		int x = s[1];
+		@endcode
+
+		@param index: Index of item to return.
+		@return Reference to item.
+	*/
+	const T& operator[](int index) const;
+
+	/*! @brief Modify the item at \p index.
+		
+		Example:
+		@code
+		SinglyLinkedList<int> s;
+		s.push(10);
+		s.push(12);
+		s[1] = 5;
+		@endcode
+
+		@param index: Index of item to modify.
+		@return Reference to item.
+	*/
+	T& operator[](int index);
+
+	/*! @brief Constructor for a new list.
+
+		Specify type in declaration, e.g.:
+		@code
+		SinglyLinkedList<int> s;
+		@endcode
+	*/
+	SinglyLinkedList();
+
+	~SinglyLinkedList();
+
+	/*! @brief Returns the size of the list.
+		@return Number of elements in the list.
+	*/
+	int size(void);
+
+	/*! @brief Prints each element of the list to stdout.
+		
+		Requires the type of list members to be compatible with stream output
+		operator ("<<").
+	*/
+	void print(void);
+
+	/*! @brief Inserts a new item at the given index.
+		@param index: Index at which to insert item.
+		@param item: Item to insert.
+	*/
+	void insert(int index, T item);
+
+	/*! @brief Appends a new item to the end of the list.
+		@param item: Item to append.
+	*/
+    void push(T item);
+
+	/*! @brief Removes and returns the item at the given index.
+		@param index: Index of the item to remove.
+		@return The item at \p index that was removed.
+	*/
+	T remove(int index);
+
+	/*! @brief Removes and returns the last item in the list.
+
+		@return The last item in the list.
+	*/
+	T pop(void);
+
+	/*! @brief Swaps the positions of the two items at the given indices.
+
+		After the swap, the item previously at \p index1 will now be located at
+		\p index2, and the item previously at \p index2 will now be located at
+		\p index1.
+
+		@param index1: Index of the first item.
+		@param index2: Index of the second item.
+	*/
+	void swap(int index1, int index2);
+};
+
+// Template class declarations in a header are not compiled when the header
+// is included in a source file. A workaround is to include the implementation
+// source in the header, as below.
+#include "SinglyLinkedList.cpp"
+#endif
